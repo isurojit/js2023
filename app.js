383f26d63abb24451674bbc0aa97cf5823b46c73
isurojit <iamsurojit@hotmail.com>
2023-04-07 11:24:19 +0530

    shortcuts vs code

---
+++ app.js
@@ -0,0 +1,10 @@
+/*Keyboard Shortcut:
+shift-alt+right/left- traverse 
+alt+updown-move
+shift-alt+up/down- to copy updown
+ctrl+D-to select perticular word one by one
+ctrl+shift+l -to select perticular word all together
+ctrl+b- toggele sidebar
+ctrl+~ toggle cmd line
+*/
+// Varibales
